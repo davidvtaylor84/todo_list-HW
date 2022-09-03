53c23512a4137a68122928f6ddf9a04d5b8e1afb
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-09-03 16:46:33 +0100

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [toDos, setToDos] = useState(["cleanse the forests", "digest the walls", "eat the future", "divulge our secrets"])
+
+  const [newToDo, setNewToDo] = useState("")
+
+  const toDoNodes = toDos.map((toDo, index)=>{
+    return(
+      <li key={index}><span>{toDo}</span></li>
+    )
+  })
+
+  const handleInput = (event)=>{
+    setNewToDo(event.target.value)
+  }
+
+  const saveNewToDo = (event)=>{
+    event.preventDefault();
+    const copyToDos = [...toDos]
+    copyToDos.push(newToDo)
+    setToDos(copyToDos)
+    setNewToDo("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ul>
+        {toDoNodes}
+      </ul>
+
+      <form onSubmit={saveNewToDo}>
+        <label htmlFor='new-todo'>Add a new mission:</label>
+        <input id='new-todo' type="text" value={newToDo} onChange={handleInput}/>
+        <input type="submit" value="Save new mission"/>
+      </form>
+
     </div>
   );
 }
