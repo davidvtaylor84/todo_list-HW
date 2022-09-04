712b2e80a760d42495244d919ad9907687ec8b7f
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-09-04 14:37:54 +0100

    added radio buttons. they don't work.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [toDos, setToDos] = useState(["cleanse the forests", "digest the walls", "eat the future", "divulge our secrets"])
+  const [toDos, setToDos] = useState(
+    [{name:"cleanse the forests", priority:"high"}, 
+    {name:"digest the walls", priority:"low"},
+    {name: "eat the future", priority:"high"}, 
+    {name:"divulge our secrets", priority:"low"}])
 
   const [newToDo, setNewToDo] = useState("")
 
   const toDoNodes = toDos.map((toDo, index)=>{
     return(
-      <li key={index}><span>{toDo}</span></li>
+      <li key={index}><span>{toDo.name}</span></li>
     )
   })
 
@@ -17,15 +21,25 @@ function App() {
     setNewToDo(event.target.value)
   }
 
+  const handlePriority =(event)=>{
+    setNewToDo(event.target.value)
+  }
+
+  // const handlePriority = (event)=>{
+  //   let toDo = newToDo
+  //   toDo.priority[event.target.value] = event.target.checked
+  // }
+
   const saveNewToDo = (event)=>{
     event.preventDefault();
     const copyToDos = [...toDos]
-    copyToDos.push(newToDo)
+    copyToDos.push({name: newToDo, priority: "Low"})
     setToDos(copyToDos)
     setNewToDo("")
   }
 
   return (
+    <>
     <div className='App'>
       <ul>
         {toDoNodes}
@@ -33,12 +47,18 @@ function App() {
 
       <form onSubmit={saveNewToDo}>
         <label htmlFor='new-todo'>Add a new mission:</label>
-        <input id='new-todo' type="text" value={newToDo} onChange={handleInput}/>
+        <input id='new-todo' type="text" value={newToDo.name} onChange={handleInput}/>
+        <input type="radio" id='new-todo' value="High" name='priority' checked={newToDo.priority === "High"} onChange={()=>handlePriority("Low")}/>
+        <label htmlFor='new-todo' for="new-todo">High</label>
+        <input type="radio" id='new-todo' value="Low" checked={newToDo.priority === "Low"} name='priority' onChange={()=>handlePriority("Low")}/>
+        <label htmlFor='new-todo' for="new-todo">Low</label>
         <input type="submit" value="Save new mission"/>
       </form>
 
     </div>
+    </>
   );
 }
 
 export default App;
+
